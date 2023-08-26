be8f84524a3d4413ea8b3870a4c80e4fc8625d4e
Oscar Ramos <64324454+lacroix021@users.noreply.github.com>
2023-08-26 18:34:58 -0500

    updated timer

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -29,7 +29,7 @@ export default function NavBar({ arrayLinks, setShowLoader }) {
         setTimeout(() => {
             document.body.classList.remove('modal-open');
             setShowLoader(false);
-        }, 5000);
+        }, 1000);
 
         setShowMenu('');
     }
